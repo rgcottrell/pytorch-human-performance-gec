04fdd33854fa34ac231c75bad1aca8823104bc44
Tianfei Chen <41580860+tianfeichen@users.noreply.github.com>
2018-11-08 07:48:25 +1030

    Bring back LSTM train script and add CONV seq2seq train script side by side.

--- fairseq-scripts/train-lang8.bat
+++ fairseq-scripts/train-lang8.bat
@@ -1,16 +1,10 @@
 python ..\fairseq\train.py^
     ..\data-bin\lang-8-fairseq^
-    --save-dir ..\checkpoints\lang-8-fairseq^
-    --arch fconv^
-    --encoder-embed-dim 500^
-    --decoder-embed-dim 500^
-    --decoder-out-embed-dim 500^
-    --encoder-layers "[(1024, 3)] * 7"^
-    --decoder-layers "[(1024, 3)] * 7"^
-    --optimizer nag^
-    --momentum 0.99^
+    --save-dir ..\checkpoints\lang-8-fairseq
     --lr 0.25^
-    --dropout 0.2^
-    --max-tokens 3000^
-    --max-sentences 20^
     --clip-norm 0.1^
+    --dropout 0.2^
+    --max-tokens 2000^
+    --max-sentences 32^
+    --arch lstm^
+    --fp16^
