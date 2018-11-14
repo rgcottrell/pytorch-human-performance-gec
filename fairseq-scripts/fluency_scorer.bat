5ab6c6dc4fb5c4b936aa8d2c2246a3896ee559ea
Robert Cottrell <rgcottrell@gmail.com>
2018-11-13 21:06:21 -0800

    Add fluency scorer script to evaluate sentences

---
+++ fairseq-scripts/fluency_scorer.bat
@@ -0,0 +1,7 @@
+python eval_fluency.py^
+    ..\data-bin\wiki103^
+    --path ..\data-bin\wiki103\wiki103.pt^
+    --raw-text^
+    --sample-break-mode eos^
+    --output-word-probs^
+    --gen-subset test
